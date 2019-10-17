d55d8d52ede14c9f461c436cdbb79f4f3fa7b34c
jingjiangtao <15210717655@163.com>
2019-10-17 15:34:23 +0800

    完成GameController的unirx重构

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UniRx;
+using System;
+using Random = UnityEngine.Random;
 
 public class GameController : MonoBehaviour
 {
-	public GameObject hazard;
-	public Vector3 spawnValues;
-	public int hazardCount;
-	public float spawnWait;
-	public float startWait;
-	public float waveWait = 2.0f;
-	public Text scoreText;
-	public Text gameOverText;
-	public Text restartText;
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float waveWait = 2.0f;
+    public Text scoreText;
+    public Text gameOverText;
+    public Text restartText;
 
-	private Vector3 spawnPosition = Vector3.zero;
-	private Quaternion spawnRotation;
-	private int score;
-	private bool gameOver;
-	private bool restart;
+    private Vector3 spawnPosition = Vector3.zero;
+    private Quaternion spawnRotation;
+    private int score;
+    private bool gameOver;
+    private bool restart;
 
     void Start()
     {
@@ -30,57 +32,46 @@ public class GameController : MonoBehaviour
         gameOver = false;
         restartText.text = string.Empty;
         restart = false;
-        StartCoroutine(SpawnWaves());
-    }
 
-    private void Update()
-    {
-        if (restart)
-        {
-            if (Input.GetKeyDown(KeyCode.R))
+        // 生成小行星
+        var spawnStream = Observable.Interval(TimeSpan.FromSeconds(spawnWait))
+            .Where(_ => !gameOver)
+            .Do(_ =>
             {
-                SceneManager.LoadScene(0);
-            }
-        }
+                spawnPosition.x = Random.Range(-spawnValues.x, spawnValues.x);
+                spawnPosition.z = spawnValues.z;
+                spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+            })
+            .Take(hazardCount)
+            .Delay(TimeSpan.FromSeconds(waveWait))
+            .RepeatSafe();
+        spawnStream.Subscribe().AddTo(this);
+
+        // 重新开始游戏
+        var restartStream = Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.R))
+            .Where(_ => restart);
+        restartStream.Subscribe(_ => SceneManager.LoadScene(0));
     }
 
-    IEnumerator SpawnWaves()
-	{
-		yield return new WaitForSeconds(startWait);
-		while (true)
-		{
-			if (gameOver)
-			{				
-				break;
-			}
-			for (int i = 0; i < hazardCount; i++)
-			{
-				spawnPosition.x = Random.Range(-spawnValues.x, spawnValues.x);
-				spawnPosition.z = spawnValues.z;
-				spawnRotation = Quaternion.identity;
-				Instantiate(hazard, spawnPosition, spawnRotation);
-				yield return new WaitForSeconds(spawnWait);
-			}
-			yield return new WaitForSeconds(waveWait);
-		}
-	}
 
-	public void AddScore(int newScoreValue)
-	{
-		score += newScoreValue;
-		UpdateScore();
-	}
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
 
-	private void UpdateScore()
-	{
-		scoreText.text = $"得分: {score}";
-	}
+    private void UpdateScore()
+    {
+        scoreText.text = $"得分: {score}";
+    }
 
-	public void GameOver()
-	{
-		gameOver = true;
-		gameOverText.text = "游戏结束";
-		restartText.text = "按R键重新开始";
-		restart = true;
-	}
+    public void GameOver()
+    {
+        gameOver = true;
+        gameOverText.text = "游戏结束";
+        restartText.text = "按R键重新开始";
+        restart = true;
+    }
 }
